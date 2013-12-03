d36574805d09410419348c7ae3d65365a8fdd26d
Arnaud Giersch <arnaud.giersch@iut-bm.univ-fcomte.fr>
2013-12-03 16:45:23 +0100

    Add some words on installation.
    Rename INSTALL script to install.mk.

---
+++ install.mk
@@ -0,0 +1,28 @@
+#!/usr/bin/make -f
+
+#DEST = $(HOME)/CoursInfo/Algo_S1/tp/graph
+DEST = $(HOME)/public_html/enseignement/Permanent/AlgoProg/tp/graph
+
+SRC = DrawingWindow.h DrawingWindow.cpp
+SRC += exemple.cpp exemple.pro
+#SRC += index.html
+
+.PHONY: help generate install install_www
+
+help:
+	@echo "Usage: ./INSTALL generate | install | install_www"
+
+generate:
+	rm -fr html/
+	doxygen Doxyfile
+	sed -i '/^pre\.fragment {/,/}/{/font-size/d}' html/doxygen.css
+
+install: generate
+	rm -fr $(DEST)
+	install -m 755 -d $(DEST)
+	install -m 644 $(SRC) $(DEST)
+	install -m 644 html/* $(DEST)
+	install -m 644 htaccess $(DEST)/.htaccess
+
+install_www: install
+	update-www
